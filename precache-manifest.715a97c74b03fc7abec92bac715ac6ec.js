a20f62ba425a197c38182d0f67ca99cd303c1ee0
vijayyennam <vjay11yenam@gmail.com>
2019-03-15 16:32:12 +0530

    Updates

---
+++ precache-manifest.715a97c74b03fc7abec92bac715ac6ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0385a63f7458ea286d2b",
+    "url": "/todo-app-client/static/js/runtime~main.0385a63f.js"
+  },
+  {
+    "revision": "d752456c50b1eb43f181",
+    "url": "/todo-app-client/static/js/main.d752456c.chunk.js"
+  },
+  {
+    "revision": "1f73dbbe82f2f12ee274",
+    "url": "/todo-app-client/static/js/1.1f73dbbe.chunk.js"
+  },
+  {
+    "revision": "d752456c50b1eb43f181",
+    "url": "/todo-app-client/static/css/main.d1da9a0b.chunk.css"
+  },
+  {
+    "revision": "2627cfe25263dacb8abd9374720622b5",
+    "url": "/todo-app-client/index.html"
+  }
+];
